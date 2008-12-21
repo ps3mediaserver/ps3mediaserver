ec18d8f234f22628df76f15563d1456c2b761ff6
ps3mediaserver <ps3mediaserver@gmail.com>
2008-12-21 16:12:23 +0000

    Shell update

--- ps3mediaserver/PMS.sh
+++ ps3mediaserver/PMS.sh
@@ -35,7 +35,7 @@ if [ "x$JAVA" = "x" ]; then
 fi
 
 # Setup the classpath
-PMS_CLASSPATH=".:$PMS_HOME/lib/pms.jar:$PMS_HOME/lib/commons-logging-api-1.0.4.jar:$PMS_HOME/lib/nanoxml-2.2.3.jar:$PMS_HOME/lib/java-unrar-0.2.jar:$PMS_HOME/lib/entagged-cvs.jar:$PMS_HOME/lib/forms-1.2.1.jar:$PMS_HOME/lib/looks-2.2.1.jar:$PMS_HOME/lib/jna.jar"
+PMS_JAR="$PMS_HOME/lib/pms.jar"
 
 # For Cygwin, switch paths to Windows format before running java
 if $cygwin; then
@@ -45,5 +45,5 @@ if $cygwin; then
 fi
 
 # Execute the JVM
-exec "$JAVA" $JAVA_OPTS -Xmx768M -Djava.encoding=UTF-8 -classpath $PMS_CLASSPATH net.pms.PMS "$@"
+exec "$JAVA" $JAVA_OPTS -Xmx768M -Djava.encoding=UTF-8 -Djava.net.preferIPv4Stack=true -jar $PMS_JAR "$@"
 
