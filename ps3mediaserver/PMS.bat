026c04e477cc58e7c487e737a7f69fcea3060a71
ps3mediaserver <ps3mediaserver@gmail.com>
2009-03-09 19:55:59 +0000

    Windows backup launcher

---
+++ ps3mediaserver/PMS.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Java PS3 Media Server
+echo ---------------------
+echo In case of troubles with PMS.exe, this shell will launch ps3mediaserver in a more old fashioned way
+echo You can try to reduce the Xmx parameter value if you keep getting "Cannot create Java virtual machine" errors...
+echo Last word: You must have java installed ! http://www.java.com
+echo ------------------------------------------------
+pause
+start javaw -Xmx768M -Djava.net.preferIPv4Stack=true -Djava.encoding=UTF-8 -jar pms.jar net.pms.PMS
