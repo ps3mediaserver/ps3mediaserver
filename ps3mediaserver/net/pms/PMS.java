7bd00b8b553ecf247b56b3210a22a3b49e5ad400
SubJunk <subjunk@gmail.com>
2011-01-25 00:54:53 +0000

    Changed to new versioning system (no longer includes SVN revision number) Added instability warning in window title if the version isn't meant for release

--- ps3mediaserver/net/pms/PMS.java
+++ ps3mediaserver/net/pms/PMS.java
@@ -131,7 +131,7 @@ public class PMS {
 	/**
 	 * Version showed in the UPNP XML descriptor and logs.
 	 */
-	public static final String VERSION = "1.20.448"; //$NON-NLS-1$
+	public static final String VERSION = "1.21.0"; //$NON-NLS-1$
 	public static final String AVS_SEPARATOR = "\1"; //$NON-NLS-1$
 
 	// TODO(tcox):  This shouldn't be static
