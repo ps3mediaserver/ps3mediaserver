eaa7cbc442cb612913fe12f166ca566632ec9389
chocolateboy <chocolate@cpan.org>
2011-02-06 01:10:35 +0000

    issue 846: Remove obsolete web video code for YouTube and GameTrailers

--- ps3mediaserver/net/pms/dlna/WebVideoStream.java
+++ ps3mediaserver/net/pms/dlna/WebVideoStream.java
@@ -18,100 +18,10 @@
  */
 package net.pms.dlna;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.StringTokenizer;
-
-import net.pms.PMS;
-import net.pms.configuration.RendererConfiguration;
 import net.pms.formats.Format;
 
 public class WebVideoStream extends WebStream {
-
-	@Override
-	public InputStream getInputStream(long low, long high, double timeseek, RendererConfiguration mediaRenderer) throws IOException {
-		if (URL.toLowerCase().indexOf("youtube") > -1 && URL.toLowerCase().indexOf("?") > -1) {
-			try {
-				InputStream is = downloadAndSend(URL, false);
-				ByteArrayOutputStream bout = new ByteArrayOutputStream();
-				int n = -1;
-				byte buffer [] = new byte [4096];
-				while( (n=is.read(buffer))> -1) {
-					bout.write(buffer, 0, n);
-				}
-				is.close();
-				String content = new String(bout.toByteArray());
-				int fs = content.indexOf("swfArgs");
-				int hd = content.indexOf("isHDAvailable = true");
-				String newURL = "http://www.youtube.com/get_video%3F";
-				if (fs > -1) {
-					String seq = content.substring(fs+18, content.indexOf("}", fs));
-					seq = seq.trim();
-					StringTokenizer st = new StringTokenizer(seq, ",");
-					while (st.hasMoreTokens()) {
-						String elt = st.nextToken();
-						if (elt.startsWith(" \"video_id\""))
-						{	
-							newURL += "&video_id%3D";
-							newURL += elt.substring(14, elt.length()-1);
-							if (hd>-1)newURL += "&fmt=22";
-							else newURL += "&fmt=18";
-						}
-						else if (elt.startsWith(" \"l\""))
-						{
-							newURL += "&l=";
-							newURL += elt.substring(6, elt.length());
-							
-						}
-						else if (elt.startsWith(" \"sk\""))
-						{
-							newURL += "&sk=";
-							newURL += elt.substring(8, elt.length()-1);
-							
-						}
-						else if (elt.startsWith(" \"t\""))
-						{
-							newURL += "&t=";
-							newURL += elt.substring(7, elt.length()-1);
-						}
-						newURL = newURL.replace("=", "%3D");
-					}
-					URL = newURL;
-				}
-			} catch (IOException e) {
-				PMS.error(null, e);
-			}
-		}
-		// from issue 282... need to script this
-		else if (URL.toLowerCase().indexOf("gametrailers") > -1 && URL.toLowerCase().indexOf("?") > -1) {
-			try {
-				InputStream is = downloadAndSend(URL, false);
-				ByteArrayOutputStream bout = new ByteArrayOutputStream();
-				int n = -1;
-				byte buffer [] = new byte [4096];
-				while( (n=is.read(buffer))> -1) {
-					bout.write(buffer, 0, n);
-				}
-				is.close();
-				String content = new String(bout.toByteArray());
-				int fs = content.indexOf("http://www.gametrailers.com/download/");
-				String newURL = "";
-				if (fs > -1) {
-					newURL= content.substring(fs, content.indexOf(">", fs)-1);
-					URL = newURL.replace(".mov", ".flv");
-				}
-			} catch (IOException e) {
-				PMS.error(null, e);
-			}
-		}
-		return super.getInputStream(low, high, timeseek, mediaRenderer);
-	}
-	
-
 	public WebVideoStream(String fluxName, String URL, String thumbURL) {
 		super(fluxName, URL, thumbURL, Format.VIDEO);
 	}
-
-
 }
