1831dfce59d9eb8c25f985b2fbdc7bbab7cf3267
chocolateboy <chocolate@cpan.org>
2011-02-17 03:30:04 +0000

    issue 766: Mac builds don't support update.jar or plugins: Mac OS X: try to support plugins by using custom class loader all platforms: make plugin dir path configurable via PMS.conf

--- ps3mediaserver/net/pms/PMS.java
+++ ps3mediaserver/net/pms/PMS.java
@@ -127,7 +127,7 @@ public class PMS {
 	/**
 	 * Version showed in the UPNP XML descriptor and logs.
 	 */
-	public static final String VERSION = "1.21.0"; //$NON-NLS-1$
+	public static final String VERSION = "1.21.1"; //$NON-NLS-1$
 	public static final String AVS_SEPARATOR = "\1"; //$NON-NLS-1$
 
 	// TODO(tcox):  This shouldn't be static
