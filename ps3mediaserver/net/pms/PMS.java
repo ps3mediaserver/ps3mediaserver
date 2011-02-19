c54ac8eae79588d8b59ce187285d2d4267ed671f
SubJunk <subjunk@gmail.com>
2011-02-19 23:13:28 +0000

    Revert recent version increments

--- ps3mediaserver/net/pms/PMS.java
+++ ps3mediaserver/net/pms/PMS.java
@@ -126,7 +126,7 @@ public class PMS {
 	/**
 	 * Version showed in the UPNP XML descriptor and logs.
 	 */
-	public static final String VERSION = "1.23.0"; //$NON-NLS-1$
+	public static final String VERSION = "1.21.0"; //$NON-NLS-1$
 	public static final String AVS_SEPARATOR = "\1"; //$NON-NLS-1$
 
 	// TODO(tcox):  This shouldn't be static
