a3497b2270f1ea5894a830c9d83a888f42097736
SubJunk <subjunk@gmail.com>
2011-04-15 23:09:58 +0000

    Add development version warning back Increment version to 1.21.2

--- ps3mediaserver/net/pms/PMS.java
+++ ps3mediaserver/net/pms/PMS.java
@@ -128,7 +128,7 @@ public class PMS {
 	/**
 	 * Version showed in the UPnP XML descriptor and logs.
 	 */
-	public static final String VERSION = "1.21.1"; //$NON-NLS-1$
+	public static final String VERSION = "1.21.2"; //$NON-NLS-1$
 	public static final String AVS_SEPARATOR = "\1"; //$NON-NLS-1$
 	
 	// (innot): The logger used for all logging.
