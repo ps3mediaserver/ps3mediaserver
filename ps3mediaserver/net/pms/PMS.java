8dea2eae0f9a54746259046a6b5709fc44b87bcd
SubJunk <subjunk@gmail.com>
2011-05-25 23:18:09 +0000

    Update changelog and increment version to 1.23.0

--- ps3mediaserver/net/pms/PMS.java
+++ ps3mediaserver/net/pms/PMS.java
@@ -127,7 +127,7 @@ public class PMS {
 	/**
 	 * Version showed in the UPnP XML descriptor and logs.
 	 */
-	public static final String VERSION = "1.22.0"; //$NON-NLS-1$
+	public static final String VERSION = "1.23.0"; //$NON-NLS-1$
 	public static final String AVS_SEPARATOR = "\1"; //$NON-NLS-1$
 	// (innot): The logger used for all logging.
 	public static final Logger logger = LoggerFactory.getLogger(PMS.class);
