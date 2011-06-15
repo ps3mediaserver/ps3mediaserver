7c0ce2ec18159da956f7b3e5d14c2d924dca0293
SubJunk <subjunk@gmail.com>
2011-06-15 01:10:46 +0000

    Minor cleanups and increment version

--- ps3mediaserver/net/pms/PMS.java
+++ ps3mediaserver/net/pms/PMS.java
@@ -132,7 +132,7 @@ public class PMS {
 	/**
 	 * Version showed in the UPnP XML descriptor and logs.
 	 */
-	public static final String VERSION = "1.24.0"; //$NON-NLS-1$
+	public static final String VERSION = "1.25.0"; //$NON-NLS-1$
 	public static final String AVS_SEPARATOR = "\1"; //$NON-NLS-1$
 
 	// (innot): The logger used for all logging.
