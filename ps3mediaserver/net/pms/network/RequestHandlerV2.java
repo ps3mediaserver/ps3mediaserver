fd5eae3a3d50304850730021bf64b0cc664e6f0a
taconaut <ph.waeber@gmail.com>
2011-08-21 12:46:54 +0000

    - recognize files with extension *.vdr as MPG files (http://www.ps3mediaserver.org/forum/viewtopic.php?f=6&t=8115) - removed unused imports - updated change log

--- ps3mediaserver/net/pms/network/RequestHandlerV2.java
+++ ps3mediaserver/net/pms/network/RequestHandlerV2.java
@@ -33,7 +33,6 @@ import net.pms.external.StartStopListenerDelegate;
 import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.channel.Channel;
-import org.jboss.netty.channel.ChannelEvent;
 import org.jboss.netty.channel.ChannelFutureListener;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.ChannelStateEvent;
