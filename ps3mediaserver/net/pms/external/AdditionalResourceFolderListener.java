3bbaf1f7f2a96cc712b00aa864c7a11cecd55333
taconaut <ph.waeber@gmail.com>
2011-11-05 11:20:01 +0000

    commented oms plugin interfaces (net.pms.external)

--- ps3mediaserver/net/pms/external/AdditionalResourceFolderListener.java
+++ ps3mediaserver/net/pms/external/AdditionalResourceFolderListener.java
@@ -2,11 +2,20 @@ package net.pms.external;
 
 import net.pms.dlna.DLNAResource;
 
+/**
+ * Classes implementing this interface and packaged as pms plugins will add a
+ * single folder to every existing folder
+ */
 public interface AdditionalResourceFolderListener extends ExternalListener {
 	/**
-	 * Allows to add a virtual folder resource, similar to the #transcoded folder
-	 * @param currentResource Parent resource
-	 * @param child 
+	 * Allows to add a virtual folder resource, similar to the #transcoded
+	 * folder to every existing folder
+	 * 
+	 * @param currentResource
+	 *            Parent resource
+	 * @param child
+	 *            the DLNAResource to add
 	 */
-	public void addAdditionalFolder(DLNAResource currentResource, DLNAResource child);
+	public void addAdditionalFolder(DLNAResource currentResource,
+			DLNAResource child);
 }
