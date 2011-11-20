16f1d2d0ea43be29f987b1ec7a9767b96787692d
Patrick Atoon <patrick.atoon@gmail.com>
2011-11-20 12:19:03 +0100

    Fixes to run PMS from Eclipse with binaries (requires a "mvn clean package" first).

--- src/main/java/net/pms/configuration/MacDefaultPaths.java
+++ src/main/java/net/pms/configuration/MacDefaultPaths.java
@@ -1,6 +1,20 @@
 package net.pms.configuration;
 
+import java.io.IOException;
+
+import net.pms.util.PmsProperties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 class MacDefaultPaths implements ProgramPaths {
+	private static final Logger logger = LoggerFactory.getLogger(MacDefaultPaths.class);
+	
+	/**
+	 * General properties for the PMS project.
+	 */
+	private final PmsProperties projectProperties = new PmsProperties();
+
 	@Override
 	public String getEac3toPath() {
 		return null;
@@ -8,27 +22,27 @@ class MacDefaultPaths implements ProgramPaths {
 
 	@Override
 	public String getFfmpegPath() {
-		return "osx/ffmpeg";
+		return getBinariesPath() + "osx/ffmpeg";
 	}
 
 	@Override
 	public String getFlacPath() {
-		return "osx/flac";
+		return getBinariesPath() + "osx/flac";
 	}
 
 	@Override
 	public String getMencoderPath() {
-		return "osx/mencoder";
+		return getBinariesPath() + "osx/mencoder";
 	}
 
 	@Override
 	public String getMplayerPath() {
-		return "osx/mplayer";
+		return getBinariesPath() + "osx/mplayer";
 	}
 
 	@Override
 	public String getTsmuxerPath() {
-		return "osx/tsMuxeR";
+		return getBinariesPath() + "osx/tsMuxeR";
 	}
 
 	@Override
@@ -38,11 +52,53 @@ class MacDefaultPaths implements ProgramPaths {
 
 	@Override
 	public String getDCRaw() {
-		return "osx/dcraw";
+		return getBinariesPath() + "osx/dcraw";
 	}
 	
 	@Override
 	public String getIMConvertPath() {
-		return "osx/convert";
+		return getBinariesPath() + "osx/convert";
+	}
+
+	/**
+	 * Constructor makes sure the project properties are read
+	 */
+	public MacDefaultPaths() {
+		try {
+			// Read project properties resource file.
+			getProjectProperties().loadFromResourceFile("/resources/project.properties");
+		} catch (IOException e) {
+			logger.error("Could not load project.properties");
+		}
+	}
+	
+	/**
+	 * Returns the project properties object.
+	 *
+	 * @return The properties object.
+	 */
+	private PmsProperties getProjectProperties() {
+		return projectProperties;
+	}
+
+	/**
+	 * Returns the path where binaries can be found. This path differs between
+	 * the build phase and the test phase. The path will end with a slash unless
+	 * it is empty.
+	 *
+	 * @return The path for binaries.
+	 */
+	private String getBinariesPath() {
+		String path = getProjectProperties().get("project.binaries");
+		
+		if (path != null && !"".equals(path)) {
+			if (path.endsWith("/")) {
+				return path;
+			} else {
+				return path + "/";
+			}
+		} else {
+			return "";
+		}
 	}
 }
