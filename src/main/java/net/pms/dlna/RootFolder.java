25362aae7f70d38a433ac9d948370c0dc2abb6bd
Patrick Atoon <patrick.atoon@gmail.com>
2012-02-12 17:10:10 +0800

    Avoiding NullPointerException.

--- src/main/java/net/pms/dlna/RootFolder.java
+++ src/main/java/net/pms/dlna/RootFolder.java
@@ -554,11 +554,13 @@ public class RootFolder extends DLNAResource {
 			Process prc = Runtime.getRuntime().exec("defaults read com.apple.iApps iTunesRecentDatabases");
 			BufferedReader in = new BufferedReader(new InputStreamReader(prc.getInputStream()));
 			if ((line = in.readLine()) != null) {
-				line = in.readLine(); // we want the 2nd line
-				line = line.trim(); // remove extra spaces
-				line = line.substring(1, line.length() - 1); // remove quotes and spaces
-				URI tURI = new URI(line);
-				iTunesFile = URLDecoder.decode(tURI.toURL().getFile(), "UTF8");
+				// we want the 2nd line
+				if ((line = in.readLine()) != null) {
+					line = line.trim(); // remove extra spaces
+					line = line.substring(1, line.length() - 1); // remove quotes and spaces
+					URI tURI = new URI(line);
+					iTunesFile = URLDecoder.decode(tURI.toURL().getFile(), "UTF8");
+				}
 			}
 			if (in != null) {
 				in.close();
