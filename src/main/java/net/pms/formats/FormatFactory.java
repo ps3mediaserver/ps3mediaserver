ps3mediaserver/ps3mediaserver

commit 309414d7d3909e791b235b4aa07d7f559f232dd2

Refactoring getAssociatedExtension(String). Introducing the FormatFactory removes the PMS dependency for all classes using this method, making them easier to test.

src/main/java/net/pms/formats/FormatFactory.java
@@ -1,0 +1,86 @@
+/*
+ * PS3 Media Server, for streaming any medias to your PS3.
+ * Copyright (C) 2008  A.Brochard
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License only.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package net.pms.formats;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class matches and instantiates formats.
+ */
+public final class FormatFactory {
+	/**
+	 * Logger used for all logging.
+	 */
+	private static final Logger LOGGER = LoggerFactory.getLogger(FormatFactory.class);
+
+	/**
+	 * List of known formats.
+	 */
+	private static final Format[] FORMATS = new Format[] { new DVRMS(),
+			new FLAC(), new GIF(), new ISO(), new JPG(), new M4A(), new MKV(),
+			new MP3(), new MPG(), new OGG(), new PNG(), new RAW(), new TIF(),
+			new WAV(), new WEB() };
+
+	/**
+	 * This class is not meant to be instantiated.
+	 */
+	private FormatFactory() {
+	}
+
+	/**
+	 * Match a given filename to all known formats and return a fresh instance
+	 * of that format. Matching is done by the extension (e.g. ".gif") or
+	 * protocol (e.g. "http://") of the filename. Will return <code>null</code>
+	 * if no match can be made.
+	 * 
+	 * @param filename The filename to match.
+	 * @return The format.
+	 * @see Format#match(String)
+	 */
+	public static Format getAssociatedExtension(final String filename) {
+		for (Format ext : FORMATS) {
+			if (ext.match(filename)) {
+				LOGGER.trace("Matched format " + ext + " to \"" + filename + "\"");
+
+				// Return a fresh instance
+				return ext.duplicate();
+			}
+		}
+
+		LOGGER.trace("Could not match any format to \"" + filename + "\"");
+		return null;
+	}
+
+	/**
+	 * Returns the list of known formats.
+	 *
+	 * @return The list of known formats.
+	 */
+	public static ArrayList<Format> getExtensions() {
+		ArrayList<Format> formats = new ArrayList<Format>(Arrays.asList(FORMATS));
+
+		return formats;
+	}
+}
+

src/main/java/net/pms/PMS.java
@@ -65,6 +65,7 @@ import net.pms.external.ExternalListener;
 import net.pms.formats.DVRMS;
 import net.pms.formats.FLAC;
 import net.pms.formats.Format;
+import net.pms.formats.FormatFactory;
 import net.pms.formats.GIF;
 import net.pms.formats.ISO;
 import net.pms.formats.JPG;
@@ -192,8 +193,6 @@ public class PMS {
 	 * User friendly name for the server.
 	 */
 	private String serverName;
-
-	private ArrayList<Format> extensions;
 
 	/**
 	 * List of registered {@link Player}s.
@@ -472,12 +471,9 @@ public class PMS {
 		// wrap System.err
 		System.setErr(new PrintStream(new SystemErrWrapper(), true));
 
-		extensions = new ArrayList<Format>();
 		players = new ArrayList<Player>();
 		allPlayers = new ArrayList<Player>();
 		server = new HTTPServer(configuration.getServerPort());
-
-		registerExtensions();
 
 		/*
 		 * XXX: keep this here (i.e. after registerExtensions and before registerPlayers) so that plugins
@@ -624,27 +620,6 @@ public class PMS {
 		ProcessWrapperImpl pwinstall = new ProcessWrapperImpl(cmdArray, new OutputParams(configuration));
 		pwinstall.runInSameThread();
 		return pwinstall.isSuccess();
-	}
-
-	/**Add a known set of extensions to the extensions list.
-	 * @see PMS#init()
-	 */
-	private void registerExtensions() {
-		extensions.add(new WEB());
-		extensions.add(new MKV());
-		extensions.add(new M4A());
-		extensions.add(new MP3());
-		extensions.add(new ISO());
-		extensions.add(new MPG());
-		extensions.add(new WAV());
-		extensions.add(new JPG());
-		extensions.add(new OGG());
-		extensions.add(new PNG());
-		extensions.add(new GIF());
-		extensions.add(new TIF());
-		extensions.add(new FLAC());
-		extensions.add(new DVRMS());
-		extensions.add(new RAW());
 	}
 
 	/**Register a known set of audio/video transcoders (known as {@link Player}s). Used in PMS#init().
@@ -945,18 +920,15 @@ public class PMS {
 	}
 
 	/**
+	 * @deprecated Use {@link FormatFactory#getAssociatedExtension(filename)}
+	 * instead.
+	 *
 	 * @param filename
 	 * @return The format.
 	 */
+	@Deprecated
 	public Format getAssociatedExtension(String filename) {
-		logger.trace("Search extension for " + filename);
-		for (Format ext : extensions) {
-			if (ext.match(filename)) {
-				logger.trace("Found 1! " + ext.getClass().getName());
-				return ext.duplicate();
-			}
-		}
-		return null;
+		return FormatFactory.getAssociatedExtension(filename);
 	}
 
 	public Player getPlayer(Class<? extends Player> profileClass, Format ext) {
@@ -1039,8 +1011,13 @@ public class PMS {
 		return server;
 	}
 
+	/**
+	 * @deprecated Use {@link FormatFactory#getExtensions()} instead.
+	 *
+	 * @return The list of formats. 
+	 */
 	public ArrayList<Format> getExtensions() {
-		return extensions;
+		return FormatFactory.getExtensions();
 	}
 
 	public ArrayList<Player> getPlayers() {

src/main/java/net/pms/dlna/DLNAResource.java
@@ -25,6 +25,7 @@ import static net.pms.util.StringUtil.encodeXML;
 import static net.pms.util.StringUtil.endTag;
 import static net.pms.util.StringUtil.openTag;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PipedInputStream;
@@ -56,6 +57,7 @@ import net.pms.external.ExternalFactory;
 import net.pms.external.ExternalListener;
 import net.pms.external.StartStopListener;
 import net.pms.formats.Format;
+import net.pms.formats.FormatFactory;
 import net.pms.io.OutputParams;
 import net.pms.io.ProcessWrapper;
 import net.pms.io.SizeLimitInputStream;
@@ -853,7 +855,7 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 
 	protected void checktype() {
 		if (getExt() == null) {
-			setExt(PMS.get().getAssociatedExtension(getSystemName()));
+			setExt(FormatFactory.getAssociatedExtension(getSystemName()));
 		}
 		if (getExt() != null) {
 			if (getExt().isUnknown()) {

src/main/java/net/pms/dlna/DVDISOTitle.java
@@ -25,16 +25,15 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
-
 import net.pms.PMS;
+import net.pms.configuration.RendererConfiguration;
+import net.pms.formats.FormatFactory;
 import net.pms.io.OutputParams;
 import net.pms.io.ProcessWrapperImpl;
 import net.pms.util.FileUtil;
 import net.pms.util.ProcessUtil;
-// Ditlew
-import net.pms.configuration.RendererConfiguration;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -233,7 +232,7 @@ public class DVDISOTitle extends DLNAResource {
 	@Override
 	public boolean isValid() {
 		if (getExt() == null) {
-			setExt(PMS.get().getAssociatedExtension("dummy.iso"));
+			setExt(FormatFactory.getAssociatedExtension("dummy.iso"));
 		}
 		return true;
 	}

src/main/java/net/pms/dlna/MapFile.java
@@ -32,6 +32,7 @@ import net.pms.PMS;
 import net.pms.configuration.MapFileConfiguration;
 import net.pms.dlna.virtual.TranscodeVirtualFolder;
 import net.pms.dlna.virtual.VirtualFolder;
+import net.pms.formats.FormatFactory;
 import net.pms.network.HTTPResource;
 import net.pms.util.NaturalComparator;
 
@@ -92,7 +93,7 @@ public class MapFile extends DLNAResource {
 			File children[] = f.listFiles();
 			for (File child : children) {
 				if (child.isFile()) {
-					if (PMS.get().getAssociatedExtension(child.getName()) != null || isFileRelevant(child)) {
+					if (FormatFactory.getAssociatedExtension(child.getName()) != null || isFileRelevant(child)) {
 						excludeNonRelevantFolder = false;
 						break;
 					}
@@ -264,7 +265,7 @@ public class MapFile extends DLNAResource {
 		}
 		for (File f : files) {
 			if (!f.isHidden()) {
-				if (f.isDirectory() || PMS.get().getAssociatedExtension(f.getName()) != null) {
+				if (f.isDirectory() || FormatFactory.getAssociatedExtension(f.getName()) != null) {
 					addedFiles.add(f);
 				}
 			}

src/main/java/net/pms/dlna/RealFile.java
@@ -27,11 +27,11 @@ import java.util.ArrayList;
 
 import net.pms.PMS;
 import net.pms.formats.Format;
+import net.pms.formats.FormatFactory;
 import net.pms.util.FileUtil;
 import net.pms.util.ProcessUtil;
 
 import org.apache.commons.lang.StringUtils;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -134,7 +134,7 @@ public class RealFile extends MapFile {
 	@Override
 	protected void checktype() {
 		if (getExt() == null) {
-			setExt(PMS.get().getAssociatedExtension(getFile().getAbsolutePath()));
+			setExt(FormatFactory.getAssociatedExtension(getFile().getAbsolutePath()));
 		}
 
 		super.checktype();

src/main/java/net/pms/dlna/virtual/VirtualVideoAction.java
@@ -21,8 +21,8 @@ package net.pms.dlna.virtual;
 import java.io.IOException;
 import java.io.InputStream;
 
-import net.pms.PMS;
 import net.pms.dlna.DLNAResource;
+import net.pms.formats.FormatFactory;
 import net.pms.network.HTTPResource;
 
 /**
@@ -158,7 +158,7 @@ public abstract class VirtualVideoAction extends DLNAResource {
 	 */
 	@Override
 	public boolean isValid() {
-		setExt(PMS.get().getAssociatedExtension("toto.mpg"));
+		setExt(FormatFactory.getAssociatedExtension("toto.mpg"));
 		return true;
 	}
 }

src/main/java/net/pms/formats/Format.java
@@ -228,4 +228,13 @@ public abstract class Format implements Cloneable {
 		}
 		return false;
 	}
+
+	/**
+	 * Return the class name for string representation.
+	 * @return The name.
+	 */
+	public String toString() {
+		return getClass().getSimpleName();
+		
+	}
 }

commit abc6f15f950baeed0faab2b45e2c4918d3705238

Fix for regression breaking plugins that change extensions.
Regression was introduced in 309414d7d3909e791b235b4aa07d7f559f232dd2,
see the discussion on the forum:
http://www.ps3mediaserver.org/forum/viewtopic.php?f=14&t=9798&p=65387#p65371

src/main/java/net/pms/formats/FormatFactory.java
@@ -35,13 +35,18 @@ public final class FormatFactory {
 	private static final Logger LOGGER = LoggerFactory.getLogger(FormatFactory.class);
 
 	/**
-	 * List of known formats.
+	 * Initial list of known formats.
 	 */
 	private static final Format[] FORMATS = new Format[] { new DVRMS(),
 			new FLAC(), new GIF(), new ISO(), new JPG(), new M4A(), new MKV(),
 			new MP3(), new MPG(), new OGG(), new PNG(), new RAW(), new TIF(),
 			new WAV(), new WEB() };
 
+	/**
+	 * The list of registered formats.
+	 */
+	private static ArrayList<Format> formats = new ArrayList<Format>(Arrays.asList(FORMATS));
+	
 	/**
 	 * This class is not meant to be instantiated.
 	 */
@@ -59,7 +64,7 @@ public final class FormatFactory {
 	 * @see Format#match(String)
 	 */
 	public static Format getAssociatedExtension(final String filename) {
-		for (Format ext : FORMATS) {
+		for (Format ext : formats) {
 			if (ext.match(filename)) {
 				LOGGER.trace("Matched format " + ext + " to \"" + filename + "\"");
 
@@ -78,9 +83,16 @@ public final class FormatFactory {
 	 * @return The list of known formats.
 	 */
 	public static ArrayList<Format> getExtensions() {
-		ArrayList<Format> formats = new ArrayList<Format>(Arrays.asList(FORMATS));
-
 		return formats;
+	}
+
+	/**
+	 * Sets the list of known formats.
+	 *
+	 * @param formatList The list of known formats.
+	 */
+	public static void setExtensions(ArrayList<Format> formatList) {
+		formats = formatList;
 	}
 }
 