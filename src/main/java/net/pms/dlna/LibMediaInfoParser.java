4e9abe560a634d1e8ecc11f0d0a5e3550144e091
Happy-Neko <happy.neko@gmail.com>
2012-07-07 12:07:20 +0400

    SubtitleType.UNKNOWN is used to identify generic sub track instead of EMBEDDED

--- src/main/java/net/pms/dlna/LibMediaInfoParser.java
+++ src/main/java/net/pms/dlna/LibMediaInfoParser.java
@@ -230,9 +230,6 @@ public class LibMediaInfoParser {
 		if (currentSubTrack.getLang() == null) {
 			currentSubTrack.setLang(DLNAMediaLang.UND);
 		}
-		if (currentSubTrack.getType() == SubtitleType.UNKNOWN) {
-			currentSubTrack.setType(SubtitleType.EMBEDDED);
-		}
 		media.getSubtitleTracksList().add(currentSubTrack);
 	}
 
