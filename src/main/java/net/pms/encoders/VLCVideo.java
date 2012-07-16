25a4c2110c777b30365b5d360b34730b489101d3
Leon Blakey <lord.quackstar@gmail.com>
2012-07-16 14:40:44 -0400

    Code reformat

--- src/main/java/net/pms/encoders/VLCVideo.java
+++ src/main/java/net/pms/encoders/VLCVideo.java
@@ -19,11 +19,16 @@
 package net.pms.encoders;
 
 import com.sun.jna.Platform;
-import java.awt.Color;
-import java.awt.GridLayout;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import net.pms.configuration.PmsConfiguration;
 import net.pms.dlna.DLNAMediaInfo;
 import net.pms.dlna.DLNAResource;
@@ -32,15 +37,6 @@ import net.pms.io.OutputParams;
 import net.pms.io.PipeProcess;
 import net.pms.io.ProcessWrapper;
 import net.pms.io.ProcessWrapperImpl;
-
-import javax.swing.*;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import javax.swing.border.TitledBorder;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import net.pms.network.HTTPResource;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
@@ -81,7 +77,7 @@ public class VLCVideo extends Player {
 	public String id() {
 		return ID;
 	}
-	
+
 	@Override
 	public boolean isTimeSeekable() {
 		return true;
@@ -124,36 +120,36 @@ public class VLCVideo extends Player {
 		//Xbox: wmv2, wma, asf (WORKING)
 		//PS3: mp1v, mpga, mpeg1 (WORKING)
 		List<String> args = new ArrayList<String>();
-		
+
 		//Codecs to use
 		args.add("venc=ffmpeg");
 		args.add("vcodec=" + codecVideo.getText());
 		args.add("acodec=" + codecAudio.getText());
-		
+
 		//Bitrate in kbit/s (TODO: Use global option?)
 		args.add("vb=4096");
 		args.add("ab=128");
-		
+
 		//Video scaling
 		args.add("scale=" + scale.getText());
-		
+
 		//Channels (TODO: is this nessesary?)
 		args.add("channels=2");
-		
+
 		//Static sample rate
 		args.add("samplerate=48000");
-		
+
 		//Stream subtitiles to client
 		//args.add("scodec=dvbs");
 		//args.add("senc=dvbsub");
-		
+
 		//Hardcode subtitiles into video
 		args.add("soverlay");
-		
+
 		//This has caused garbled audio, so only enable when told to
-		if(audioSyncEnabled.isSelected())
+		if (audioSyncEnabled.isSelected())
 			args.add("audio-sync");
-		
+
 		return args;
 	}
 
@@ -182,34 +178,34 @@ public class VLCVideo extends Player {
 		cmdList.add("dummy");
 
 		//Hardware accelleration seems to be more stable now, so its enabled
-		if(hardwareAccel.isSelected())
+		if (hardwareAccel.isSelected())
 			cmdList.add("--ffmpeg-hw");
-		
+
 		//Useful for the more esoteric codecs people use
-		if(experimentalCodecs.isSelected())
+		if (experimentalCodecs.isSelected())
 			cmdList.add("--sout-ffmpeg-strict=-2");
 
 		//Stop the DOS box from appearing on windows
 		if (isWindows)
 			cmdList.add("--dummy-quiet");
-		
+
 		//File needs to be given before sout, otherwise vlc complains
 		cmdList.add(fileName);
-		
-		if(subtitleEnabled.isSelected())
+
+		if (subtitleEnabled.isSelected())
 			cmdList.add("--sub-language=" + subtitlePri.getText());
 		cmdList.add("--audio-language=" + audioPri.getText());
-		
+
 		//Add our transcode options
 		String transcodeSpec = String.format(
-			"#transcode{%s}:std{access=file,mux=%s,dst=\"%s%s\"}",
-			StringUtils.join(getEncodingArgs(), ","),
-			getMux(),
-			(isWindows ? "\\\\" : ""),
-			tsPipe.getInputPipe());
+				"#transcode{%s}:std{access=file,mux=%s,dst=\"%s%s\"}",
+				StringUtils.join(getEncodingArgs(), ","),
+				getMux(),
+				(isWindows ? "\\\\" : ""),
+				tsPipe.getInputPipe());
 		cmdList.add("--sout");
 		cmdList.add(transcodeSpec);
-		
+
 		//Force VLC to die when finished
 		cmdList.add("vlc://quit");
 
@@ -237,89 +233,89 @@ public class VLCVideo extends Player {
 		//Working impl in swing in 6 mins > semi-working impl in jgoodies in 6 hours
 		JPanel mainPanel = new JPanel();
 		TitledBorder titledBorder = BorderFactory.createTitledBorder("VLC Transcoder Settings");
-        titledBorder.setTitleJustification(TitledBorder.LEFT);
+		titledBorder.setTitleJustification(TitledBorder.LEFT);
 		mainPanel.setBorder(titledBorder);
 		//Yes this is ugly, 
 		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-		
+
 		hardwareAccel = new JCheckBox("Use hardware acceleration");
 		hardwareAccel.setContentAreaFilled(false);
 		mainPanel.add(hardwareAccel);
-		
+
 		experimentalCodecs = new JCheckBox("Enable experimental codecs");
 		experimentalCodecs.setContentAreaFilled(false);
 		mainPanel.add(experimentalCodecs);
-		
+
 		audioSyncEnabled = new JCheckBox("Enable audio sync");
 		audioSyncEnabled.setContentAreaFilled(false);
 		mainPanel.add(audioSyncEnabled);
-		
+
 		//Try adding a label with a text field
 		audioPri = genTextField("Audio Language Priority", "jpn,eng", mainPanel);
 		subtitleEnabled = new JCheckBox("Enable Subtitles");
 		subtitleEnabled.setSelected(true);
 		mainPanel.add(subtitleEnabled);
 		subtitlePri = genTextField("Subtitle Language Priority", "eng,jpn", mainPanel);
-		
+
 		//Add slider for scale
 		JPanel sliderPanel = new JPanel();
 		sliderPanel.add(new JLabel("Video scale: "));
 		scale = new JTextField("" + scaleDefault);
 		sliderPanel.add(scale);
-		final JSlider scaleSlider = new JSlider(JSlider.HORIZONTAL, 0, 10, (int)(scaleDefault*10));
+		final JSlider scaleSlider = new JSlider(JSlider.HORIZONTAL, 0, 10, (int) (scaleDefault * 10));
 		Hashtable scaleLabels = new Hashtable();
-		scaleLabels.put(0, new JLabel("0.0") );
-		scaleLabels.put(5, new JLabel("0.5") );
-		scaleLabels.put(10, new JLabel("1.0") );
+		scaleLabels.put(0, new JLabel("0.0"));
+		scaleLabels.put(5, new JLabel("0.5"));
+		scaleLabels.put(10, new JLabel("1.0"));
 		scaleSlider.setLabelTable(scaleLabels);
 		scaleSlider.addChangeListener(new ChangeListener() {
 			@Override
 			public void stateChanged(ChangeEvent ce) {
-				scale.setText(String.valueOf((double)scaleSlider.getValue()/10));
+				scale.setText(String.valueOf((double) scaleSlider.getValue() / 10));
 			}
 		});
 		scale.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyReleased(KeyEvent e) {
 				String typed = scale.getText();
-                scaleSlider.setValue(0);
-                if(!typed.matches("\\d+") || typed.length() > 3)
-                    return;
-                scaleSlider.setValue(Integer.parseInt(typed)*10);
+				scaleSlider.setValue(0);
+				if (!typed.matches("\\d+") || typed.length() > 3)
+					return;
+				scaleSlider.setValue(Integer.parseInt(typed) * 10);
 			}
 		});
 		sliderPanel.add(scaleSlider);
 		mainPanel.add(sliderPanel);
-		
+
 		//Allow user to choose codec
 		JPanel codecPanel = new JPanel();
-		codecPanel.add(new JLabel("<html>Codecs that VLC will use. <br>Good places to start:" +
-				"<br> XBox: wmv2, wma, asf" +
-				"<br> PS3: mp1v, mpga, mpeg</html>"));
+		codecPanel.add(new JLabel("<html>Codecs that VLC will use. <br>Good places to start:"
+				+ "<br> XBox: wmv2, wma, asf"
+				+ "<br> PS3: mp1v, mpga, mpeg</html>"));
 		codecVideo = genTextField("Video codec: ", "wmv2", codecPanel, 6);
 		codecAudio = genTextField("Audio codec: ", "wma", codecPanel, 6);
 		codecContainer = genTextField("Container: ", "asf", codecPanel, 6);
 		mainPanel.add(codecPanel);
-		
+
 		return mainPanel;
 	}
-	
+
 	protected JTextField genTextField(String labelText, String fieldText, JPanel target) {
 		return genTextField(labelText, fieldText, target, 20);
 	}
-	
+
 	protected JTextField genTextField(String labelText, String fieldText, JPanel target, int columns) {
 		JPanel container = new JPanel();
 		JLabel label = new JLabel(labelText);
 		JTextField field = new JTextField(fieldText);
 		field.setColumns(columns);
 		label.setLabelFor(field);
-	
+
 		container.add(label);
 		container.add(field);
-		
+
 		target.add(container);
-		
+
 		return field;
 	}
 }
