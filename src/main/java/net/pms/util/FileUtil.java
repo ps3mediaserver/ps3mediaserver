caa6f9f1aeb42d71e6bd642e4d1d53df91b3ce6c
Happy-Neko <happy.neko@gmail.com>
2012-07-23 02:42:02 +0400

    isFileUTF8() and isFileUTF16() added to FileUtil

--- src/main/java/net/pms/util/FileUtil.java
+++ src/main/java/net/pms/util/FileUtil.java
@@ -5,6 +5,7 @@ import net.pms.dlna.DLNAMediaInfo;
 import net.pms.dlna.DLNAMediaSubtitle;
 import net.pms.formats.v2.SubtitleType;
 import org.apache.commons.lang.StringUtils;
+import org.mozilla.universalchardet.Constants;
 import org.mozilla.universalchardet.UniversalDetector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -208,5 +209,25 @@ public class FileUtil {
 		universalDetector.reset();
 
 		return encoding;
+	}
+
+	/**
+	 * Tests if file is UTF-8 encoded with or without BOM.
+	 * @param file File to test
+	 * @return true if file is UTF-8 encoded with or without BOM, false otherwise.
+	 * @throws IOException
+	 */
+	public static boolean isFileUTF8(File file) throws IOException {
+		return getFileCharset(file) == Constants.CHARSET_UTF_8;
+	}
+
+	/**
+	 * Tests if file is UTF-16 encoded LE or BE.
+	 * @param file File to test
+	 * @return true if file is UTF-16 encoded LE or BE, false otherwise.
+	 * @throws IOException
+	 */
+	public static boolean isFileUTF16(File file) throws IOException {
+		return (getFileCharset(file) == Constants.CHARSET_UTF_16LE || getFileCharset(file) == Constants.CHARSET_UTF_16BE);
 	}
 }
