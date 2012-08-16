9934aeada97b28ed2c535472c4e4001c7f5081aa
smo <smoooms@googlemail.com>
2012-08-16 15:43:07 +0200

    fix directory for tsmuxer (linux only)

--- src/main/java/net/pms/configuration/LinuxDefaultPaths.java
+++ src/main/java/net/pms/configuration/LinuxDefaultPaths.java
@@ -34,7 +34,7 @@ class LinuxDefaultPaths implements ProgramPaths {
 
 	@Override
 	public String getTsmuxerPath() {
-		return getBinaryPath("tsMuxeR");
+		return getBinaryPath("linux/tsMuxeR");
 	}
 
 	@Override
