ps3mediaserver/ps3mediaserver

commit d9f89fc59737693be4dd36b8dfc132c6890aff77

work around Java file permissions bugs remove redundant file existence checks

src/main/java/net/pms/configuration/MapFileConfiguration.java
@@ -14,11 +14,17 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.pms.util.FileUtil;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  *
  * @author mfranco
  */
 public class MapFileConfiguration {
+	private static final Logger LOGGER = LoggerFactory.getLogger(MapFileConfiguration.class);
 	private String name;
 	private String thumbnailIcon;
 	private List<MapFileConfiguration> children;
@@ -59,14 +65,17 @@ public class MapFileConfiguration {
 
 	public static List<MapFileConfiguration> parse(String conf) {
 		if (conf != null && conf.startsWith("@")) {
-			File f = new File(conf.substring(1));
+			File file = new File(conf.substring(1));
 			conf = null;
-			if (f.canRead()) {
+
+			if (FileUtil.isFileReadable(file)) {
 				try {
-					conf = FileUtils.readFileToString(f);
+					conf = FileUtils.readFileToString(file);
 				} catch (IOException ex) {
 					return null;
 				}
+			} else {
+				LOGGER.warn("Can't read {}", file.getAbsolutePath());
 			}
 		}
 
@@ -77,24 +86,29 @@ public class MapFileConfiguration {
 		GsonBuilder gsonBuilder = new GsonBuilder();
 		gsonBuilder.registerTypeAdapter(File.class, new FileSerializer());
 		Gson gson = gsonBuilder.create();
-		Type listType = new TypeToken<ArrayList<MapFileConfiguration>>() {
-		}.getType();
+		Type listType = (new TypeToken<ArrayList<MapFileConfiguration>>() { }).getType();
 		List<MapFileConfiguration> out = gson.fromJson(conf, listType);
 		return out;
 	}
 }
 
 class FileSerializer implements JsonSerializer<File>, JsonDeserializer<File> {
+	private static final Logger LOGGER = LoggerFactory.getLogger(FileSerializer.class);
+
 	public JsonElement serialize(File src, Type typeOfSrc, JsonSerializationContext context) {
 		return new JsonPrimitive(src.getAbsolutePath());
 	}
 
 	public File deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-		throws JsonParseException {
-		File f = new File(json.getAsJsonPrimitive().getAsString());
-		if (!f.canRead()) {
+		throws JsonParseException
+	{
+		File file = new File(json.getAsJsonPrimitive().getAsString());
+
+		if (!FileUtil.isFileReadable(file)) {
+			LOGGER.warn("Can't read {}", file.getAbsolutePath());
 			return null;
+		} else {
+			return file;
 		}
-		return f;
 	}
 }

src/main/java/net/pms/util/FileUtil.java
@@ -4,7 +4,9 @@ import net.pms.PMS;
 import net.pms.dlna.DLNAMediaInfo;
 import net.pms.dlna.DLNAMediaSubtitle;
 import net.pms.formats.v2.SubtitleType;
+
 import org.mozilla.universalchardet.UniversalDetector;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,7 +16,6 @@ import java.util.Map;
 
 import static org.apache.commons.lang.StringUtils.*;
 import static org.mozilla.universalchardet.Constants.*;
-
 
 public class FileUtil {
 	private static final Logger LOGGER = LoggerFactory.getLogger(FileUtil.class);
@@ -40,27 +41,41 @@ public class FileUtil {
 		return f.substring(0, point);
 	}
 
-	public static File getFileNameWitNewExtension(File parent, File f, String ext) {
-		File ff = isFileExists(new File(parent, f.getName()), ext);
+	public static File getFileNameWithNewExtension(File parent, File file, String ext) {
+		File ff = isFileExists(new File(parent, file.getName()), ext);
+
 		if (ff != null && ff.exists()) {
 			return ff;
 		}
+
 		return null;
 	}
 
-	public static File getFileNameWitAddedExtension(File parent, File f, String ext) {
+	/**
+	 * @deprecated Use {@link #getFileNameWithNewExtension(File, File, String)}.
+	 */
+	@Deprecated
+	public static File getFileNameWitNewExtension(File parent, File f, String ext) {
+		return getFileNameWithNewExtension(parent, f, ext);
+	}
+
+	public static File getFileNameWithAddedExtension(File parent, File f, String ext) {
 		File ff = new File(parent, f.getName() + ext);
+
 		if (ff.exists()) {
 			return ff;
 		}
+
 		return null;
 	}
 
 	public static File isFileExists(File f, String ext) {
 		int point = f.getName().lastIndexOf(".");
+
 		if (point == -1) {
 			point = f.getName().length();
 		}
+
 		File lowerCasedFilename = new File(f.getParentFile(), f.getName().substring(0, point) + "." + ext.toLowerCase());
 		if (lowerCasedFilename.exists()) {
 			return lowerCasedFilename;
@@ -70,6 +85,7 @@ public class FileUtil {
 		if (upperCasedFilename.exists()) {
 			return upperCasedFilename;
 		}
+
 		return null;
 	}
 
@@ -154,6 +170,7 @@ public class FileUtil {
 										} catch (FileNotFoundException ex) {
 											LOGGER.warn("Exception during external subtitles scan.", ex);
 										}
+
 										exists = true;
 									}
 								}
@@ -302,6 +319,7 @@ public class FileUtil {
 		if (isCharsetUTF16(charset)) {
 			if (!outputFile.exists()) {
 				BufferedReader reader = null;
+
 				try {
 					if (equalsIgnoreCase(charset, CHARSET_UTF_16LE)) {
 						reader = new BufferedReader(new InputStreamReader(new FileInputStream(inputFile), "UTF-16"));
@@ -315,14 +333,128 @@ public class FileUtil {
 
 				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputFile), "UTF-8"));
 				int c;
+
 				while ((c = reader.read()) != -1) {
 					writer.write(c);
 				}
+
 				writer.close();
 				reader.close();
 			}
 		} else {
 			throw new IllegalArgumentException("File is not UTF-16");
 		}
+	}
+
+	/**
+	 * Determine whether a file is readable by trying to read it. This works around JDK bugs which
+	 * return the wrong results for {@link java.io.File.canRead()} on Windows, and in some cases, on Unix.
+	 * <p>
+	 * Note: since this method accesses the filesystem, it should not be used in contexts in which performance is critical.
+	 * Note: this method changes the file access time.
+	 *
+	 * @since 1.71.0
+	 * @param file the File whose permissions are to be determined
+	 * @return <code>true</code> if the file is not null, exists, is a file and can be read, <code>false</code> otherwise
+	 */
+	// based on the workaround posted here:
+	// http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4993360
+	// XXX why isn't this in Apache Commons?
+	public static boolean isFileReadable(File file) {
+		boolean canRead = false;
+
+		if (file != null) {
+			try {
+				new FileInputStream(file).close();
+				canRead = true;
+			} catch (IOException ioe) { }
+		}
+
+		return canRead;
+	}
+
+	/**
+	 * Determine whether a file is writable by trying to write it. This works around JDK bugs which
+	 * return the wrong results for {@link java.io.File.canWrite()} on Windows and, in some cases, on Unix.
+	 * <p>
+	 * Note: since this method accesses the filesystem, it should not be used in contexts in which performance is critical.
+	 * Note: this method changes the file access time and may change the file modification time.
+	 *
+	 * @since 1.71.0
+	 * @param file the File whose permissions are to be determined
+	 * @return <code>true</code> if the file is not null, exists, is a file and can be written to, <code>false</code> otherwise
+	 */
+	// Based on the workaround posted here:
+	// http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4993360
+	// XXX why isn't this in Apache Commons?
+	public static boolean isFileWritable(File file) {
+		boolean canWrite = false;
+
+		if (file != null) {
+			boolean alreadyExists = file.isFile();
+
+			try {
+				// true: open for append: make sure the open
+				// doesn't clobber the file
+				new FileOutputStream(file, true).close();
+				canWrite = true;
+
+				if (!alreadyExists) {
+					try {
+						if (!file.delete()) {;
+							LOGGER.warn("Can't delete temporary test file: {}", file.getAbsolutePath());
+						}
+					} catch (SecurityException se) {
+						LOGGER.error("Error deleting temporary test file: " + file.getAbsolutePath(), se);
+					}
+				}
+			} catch (IOException ioe) {
+			} catch (SecurityException se) { }
+		}
+
+		return canWrite;
+	}
+
+	/**
+	 * Determines whether the supplied directory is writable by trying to write a file to it.
+	 * This works around JDK bugs which return the wrong results for {@link java.io.File.canWrite()}
+	 * on Windows and possibly on Unix.
+	 * <p>
+	 * Note: since this method accesses the filesystem, it should not be used in contexts in which performance is critical.
+	 * Note: this method changes the file access time and may change the file modification time.
+	 *
+	 * @since 1.71.0
+	 * @param dir the File whose permissions are to be determined
+	 * @return <code>true</code> if the directory is not null, exists, is a directory and can be written to, <code>false</code> otherwise
+	 */
+	// XXX dir.canWrite() has issues on Windows, so verify it directly:
+	// http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6203387
+	public static boolean isDirectoryWritable(File dir) {
+		boolean isWritable = false;
+
+		if (dir != null) {
+			// new File("").isDirectory() is false, even though getAbsolutePath() returns the right path.
+			// this resolves it
+			dir = dir.getAbsoluteFile();
+
+			if (dir.isDirectory()) {
+				File file = new File(dir, String.format("pms_directory_write_test_%d.tmp", System.currentTimeMillis()));
+
+				try {
+					if (file.createNewFile()) {
+						if (isFileWritable(file)) {
+							isWritable = true;
+						}
+
+						if (!file.delete()) {
+							LOGGER.warn("Can't delete temporary test file: {}", file.getAbsolutePath());
+						}
+					}
+				} catch (IOException ioe) {
+				} catch (SecurityException se) { }
+			}
+		}
+
+		return isWritable;
 	}
 }

src/test/java/net/pms/util/FileUtilTest.java
@@ -19,15 +19,19 @@
 package net.pms.util;
 
 import org.apache.commons.io.FileUtils;
+
 import org.junit.Before;
 import org.junit.Test;
+
 import org.mozilla.universalchardet.Constants;
+
 import org.slf4j.LoggerFactory;
 
 import ch.qos.logback.classic.LoggerContext;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import static org.fest.assertions.Assertions.assertThat;
 
@@ -41,7 +45,7 @@ public class FileUtilTest {
 	public final void setUp() {
 		// Silence all log messages from the PMS code that is being tested
 		LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
-		context.reset();
+		// context.reset();
 	}
 
 	@Test
@@ -225,5 +229,63 @@ public class FileUtilTest {
 	@Test(expected = FileNotFoundException.class)
 	public void testConvertFileFromUtf16ToUtf8_inputFileNotFound() throws Exception {
 		FileUtil.convertFileFromUtf16ToUtf8(new File("no-such-file.xyz"), new File("output.srt"));
+	}
+
+	@Test
+	public void testIsFileReadable() {
+		assertThat(FileUtil.isFileReadable(null)).isFalse();
+		assertThat(FileUtil.isFileReadable(new File(""))).isFalse();
+		assertThat(FileUtil.isFileReadable(new File(System.getProperty("user.dir")))).isFalse();
+		assertThat(FileUtil.isFileReadable(new File("no such file"))).isFalse();
+
+		File file = FileUtils.toFile(CLASS.getResource("english-utf8-with-bom.srt"));
+		assertThat(FileUtil.isFileReadable(file)).isTrue();
+
+		assertThat(file.getParentFile()).isNotNull();
+		assertThat(FileUtil.isFileReadable(new File(file.getParentFile(), "no such file"))).isFalse();
+	}
+
+	@Test
+	public void testIsFileWritable() throws IOException {
+		assertThat(FileUtil.isFileWritable(null)).isFalse();
+		assertThat(FileUtil.isFileWritable(new File(""))).isFalse();
+		assertThat(FileUtil.isFileWritable(new File(System.getProperty("user.dir")))).isFalse();
+		String filename = String.format("pms_temp_writable_file_%d_1.tmp", System.currentTimeMillis());
+		assertThat(FileUtil.isFileWritable(new File(filename))).isTrue();
+
+		File file = FileUtils.toFile(CLASS.getResource("english-utf8-with-bom.srt"));
+		assertThat(FileUtil.isFileReadable(file)).isTrue();
+
+		assertThat(file.getParentFile()).isNotNull();
+
+		filename = String.format("pms_temp_writable_file_%d_2.tmp", System.currentTimeMillis());
+		File tempFile = null;
+
+		try {
+			tempFile = new File(file.getParentFile(), filename);
+			tempFile.createNewFile();
+			assertThat(file.isFile()).isTrue();
+			assertThat(FileUtil.isFileReadable(tempFile));
+			assertThat(FileUtil.isFileWritable(tempFile));
+		} finally {
+			if (tempFile != null) {
+				tempFile.delete();
+			}
+		}
+	}
+
+	@Test
+	public void testIsDirectoryWritable() {
+		assertThat(FileUtil.isDirectoryWritable(null)).isFalse();
+		assertThat(FileUtil.isDirectoryWritable(new File("no such directory"))).isFalse();
+		assertThat(FileUtil.isDirectoryWritable(FileUtils.toFile(CLASS.getResource("english-utf8-with-bom.srt")))).isFalse();
+
+		File file = FileUtils.toFile(CLASS.getResource("english-utf8-with-bom.srt"));
+		assertThat(FileUtil.isFileReadable(file)).isTrue();
+
+		File dir = file.getParentFile();
+		assertThat(dir).isNotNull();
+		assertThat(dir.isDirectory()).isTrue();
+		assertThat(FileUtil.isDirectoryWritable(dir)).isTrue();
 	}
 }

src/main/java/net/pms/PMS.java
@@ -43,6 +43,7 @@ import net.pms.network.UPNPHelper;
 import net.pms.newgui.LooksFrame;
 import net.pms.newgui.ProfileChooser;
 import net.pms.update.AutoUpdater;
+import net.pms.util.FileUtil;
 import net.pms.util.ProcessUtil;
 import net.pms.util.PropertiesUtil;
 import net.pms.util.SystemErrWrapper;
@@ -342,11 +343,12 @@ public class PMS {
 		String cwd = new File("").getAbsolutePath();
 		LOGGER.info("Working directory: " + cwd);
 
-		LOGGER.info("Temp folder: " + configuration.getTempFolder());
+		LOGGER.info("Temp directory: " + configuration.getTempFolder());
 		LOGGER.info("Logging config file: " + LoggingConfigFileLoader.getConfigFilePath());
 
 		HashMap<String, String> lfps = LoggingConfigFileLoader.getLogFilePaths();
 
+		// debug.log filename(s) and path(s)
 		if (lfps != null && lfps.size() > 0) {
 			if (lfps.size() == 1) {
 				Entry<String, String> entry = lfps.entrySet().iterator().next();
@@ -371,13 +373,13 @@ public class PMS {
 		File profileFile = new File(profilePath);
 
 		if (profileFile.exists()) {
-			String status = String.format("%s%s",
-				profileFile.canRead()  ? "r" : "-",
-				profileFile.canWrite() ? "w" : "-"
+			String permissions = String.format("%s%s",
+				FileUtil.isFileReadable(profileFile) ? "r" : "-",
+				FileUtil.isFileWritable(profileFile) ? "w" : "-"
 			);
-			LOGGER.info("Profile status: " + status);
+			LOGGER.info("Profile permissions: " + permissions);
 		} else {
-			LOGGER.info("Profile status: no such file");
+			LOGGER.info("Profile permissions: no such file");
 		}
 
 		LOGGER.info("Profile name: " + configuration.getProfileName());

src/main/java/net/pms/configuration/PmsConfiguration.java
@@ -22,6 +22,7 @@ import com.sun.jna.Platform;
 
 import net.pms.io.SystemUtils;
 import net.pms.Messages;
+import net.pms.util.FileUtil;
 import net.pms.util.PropertiesUtil;
 
 import org.apache.commons.configuration.Configuration;
@@ -296,7 +297,7 @@ public class PmsConfiguration {
 			// if it exists, we know whether it's a file or directory
 			// otherwise, it must be a file since we don't autovivify directories
 
-			if (f.exists() && f.isDirectory()) {
+			if (f.isDirectory()) {
 				PROFILE_DIRECTORY = FilenameUtils.normalize(f.getAbsolutePath());
 				PROFILE_PATH = FilenameUtils.normalize(new File(f, DEFAULT_PROFILE_FILENAME).getAbsolutePath());
 			} else { // doesn't exist or is a file (i.e. not a directory)
@@ -376,15 +377,24 @@ public class PmsConfiguration {
 		if (loadFile) {
 			File pmsConfFile = new File(PROFILE_PATH);
 
-			if (pmsConfFile.isFile() && pmsConfFile.canRead()) {
-				configuration.load(PROFILE_PATH);
+			if (pmsConfFile.isFile()) {
+				if (FileUtil.isFileReadable(pmsConfFile)) {
+					configuration.load(PROFILE_PATH);
+				} else {
+					LOGGER.warn("Can't load {}", PROFILE_PATH);
+				}
 			} else if (SKEL_PROFILE_PATH != null) {
                 File pmsSkelConfFile = new File(SKEL_PROFILE_PATH);
-                if (pmsSkelConfFile.isFile() && pmsSkelConfFile.canRead()) {
-                    // load defaults from skel file, save them later to PROFILE_PATH
-                    configuration.load(pmsSkelConfFile);
-                    LOGGER.info("Default configuration loaded from " + SKEL_PROFILE_PATH);
-                }
+
+				if (pmsSkelConfFile.isFile()) {
+					if (FileUtil.isFileReadable(pmsSkelConfFile)) {
+						// load defaults from skel file, save them later to PROFILE_PATH
+						configuration.load(pmsSkelConfFile);
+						LOGGER.info("Default configuration loaded from " + SKEL_PROFILE_PATH);
+					} else {
+						LOGGER.warn("Can't load {}", SKEL_PROFILE_PATH);
+					}
+				}
             }
 		}
 

commit b61ab64e69c64639d797b7e7a0b73d4475f99deb

FileUtil: add isDirectoryReadable to fix virtual folder breakage introduced in d9f89fc597

src/main/java/net/pms/configuration/MapFileConfiguration.java
@@ -75,7 +75,7 @@ public class MapFileConfiguration {
 					return null;
 				}
 			} else {
-				LOGGER.warn("Can't read {}", file.getAbsolutePath());
+				LOGGER.warn("Can't read file: {}", file.getAbsolutePath());
 			}
 		}
 
@@ -104,8 +104,8 @@ class FileSerializer implements JsonSerializer<File>, JsonDeserializer<File> {
 	{
 		File file = new File(json.getAsJsonPrimitive().getAsString());
 
-		if (!FileUtil.isFileReadable(file)) {
-			LOGGER.warn("Can't read {}", file.getAbsolutePath());
+		if (!FileUtil.isDirectoryReadable(file)) {
+			LOGGER.warn("Can't read directory: {}", file.getAbsolutePath());
 			return null;
 		} else {
 			return file;

src/main/java/net/pms/util/FileUtil.java
@@ -369,16 +369,16 @@ public class FileUtil {
 	// http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4993360
 	// XXX why isn't this in Apache Commons?
 	public static boolean isFileReadable(File file) {
-		boolean canRead = false;
+		boolean isReadable = false;
 
-		if (file != null) {
+		if ((file != null) && file.isFile()) {
 			try {
 				new FileInputStream(file).close();
-				canRead = true;
+				isReadable = true;
 			} catch (IOException ioe) { }
 		}
 
-		return canRead;
+		return isReadable;
 	}
 
 	/**
@@ -390,37 +390,73 @@ public class FileUtil {
 	 *
 	 * @since 1.71.0
 	 * @param file the File whose permissions are to be determined
-	 * @return <code>true</code> if the file is not null, exists, is a file and can be written to, <code>false</code> otherwise
+	 * @return <code>true</code> if the file is not null and either a) exists, is a file and can be written to or b) doesn't
+	 * exist and can be created; otherwise returns <code>false</code>
 	 */
-	// Based on the workaround posted here:
+	// Loosely based on the workaround posted here:
 	// http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4993360
 	// XXX why isn't this in Apache Commons?
 	public static boolean isFileWritable(File file) {
-		boolean canWrite = false;
+		boolean isWritable = false;
 
 		if (file != null) {
-			boolean alreadyExists = file.isFile();
+			boolean fileAlreadyExists = file.isFile(); // i.e. exists and is a File
 
-			try {
-				// true: open for append: make sure the open
-				// doesn't clobber the file
-				new FileOutputStream(file, true).close();
-				canWrite = true;
+			if (fileAlreadyExists || !file.exists()) {
+				try {
+					// true: open for append: make sure the open
+					// doesn't clobber the file
+					new FileOutputStream(file, true).close();
+					isWritable = true;
 
-				if (!alreadyExists) {
-					try {
-						if (!file.delete()) {;
-							LOGGER.warn("Can't delete temporary test file: {}", file.getAbsolutePath());
+					if (!fileAlreadyExists) { // a new file has been "touch"ed; try to remove it
+						try {
+							if (!file.delete()) {;
+								LOGGER.warn("Can't delete temporary test file: {}", file.getAbsolutePath());
+							}
+						} catch (SecurityException se) {
+							LOGGER.error("Error deleting temporary test file: " + file.getAbsolutePath(), se);
 						}
-					} catch (SecurityException se) {
-						LOGGER.error("Error deleting temporary test file: " + file.getAbsolutePath(), se);
 					}
-				}
-			} catch (IOException ioe) {
-			} catch (SecurityException se) { }
+				} catch (IOException ioe) {
+				} catch (SecurityException se) { }
+			}
 		}
 
-		return canWrite;
+		return isWritable;
+	}
+
+	/**
+	 * Determines whether the supplied directory is readable by trying to read its contents.
+	 * This works around JDK bugs which return the wrong results for {@link java.io.File.canRead()}
+	 * on Windows and possibly on Unix.
+	 * <p>
+	 * Note: since this method accesses the filesystem, it should not be used in contexts in which performance is critical.
+	 * Note: this method changes the file access time.
+	 *
+	 * @since 1.71.0
+	 * @param dir the File whose permissions are to be determined
+	 * @return <code>true</code> if the directory is not null, exists, is a directory and can be read, <code>false</code> otherwise
+	 */
+	// XXX dir.canRead() has issues on Windows, so verify it directly:
+	// http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6203387
+	public static boolean isDirectoryReadable(File dir) {
+		boolean isReadable = false;
+
+		if (dir != null) {
+			// new File("").isDirectory() is false, even though getAbsolutePath() returns the right path.
+			// this resolves it
+			dir = dir.getAbsoluteFile();
+
+			if (dir.isDirectory()) {
+				try {
+					File[] files = dir.listFiles(); // null if an I/O error occurs
+					isReadable = files != null;
+				} catch (SecurityException se) { }
+			}
+		}
+
+		return isReadable;
 	}
 
 	/**
@@ -446,7 +482,14 @@ public class FileUtil {
 			dir = dir.getAbsoluteFile();
 
 			if (dir.isDirectory()) {
-				File file = new File(dir, String.format("pms_directory_write_test_%d.tmp", System.currentTimeMillis()));
+				File file = new File(
+					dir,
+					String.format(
+						"pms_directory_write_test_%d_%d.tmp",
+						System.currentTimeMillis(),
+						Thread.currentThread().getId()
+					)
+				);
 
 				try {
 					if (file.createNewFile()) {

src/test/java/net/pms/util/FileUtilTest.java
@@ -275,6 +275,21 @@ public class FileUtilTest {
 	}
 
 	@Test
+	public void testIsDirectoryReadable() {
+		assertThat(FileUtil.isDirectoryReadable(null)).isFalse();
+		assertThat(FileUtil.isDirectoryReadable(new File("no such directory"))).isFalse();
+		assertThat(FileUtil.isDirectoryReadable(FileUtils.toFile(CLASS.getResource("english-utf8-with-bom.srt")))).isFalse();
+
+		File file = FileUtils.toFile(CLASS.getResource("english-utf8-with-bom.srt"));
+		assertThat(FileUtil.isFileReadable(file)).isTrue();
+
+		File dir = file.getParentFile();
+		assertThat(dir).isNotNull();
+		assertThat(dir.isDirectory()).isTrue();
+		assertThat(FileUtil.isDirectoryReadable(dir)).isTrue();
+	}
+
+	@Test
 	public void testIsDirectoryWritable() {
 		assertThat(FileUtil.isDirectoryWritable(null)).isFalse();
 		assertThat(FileUtil.isDirectoryWritable(new File("no such directory"))).isFalse();