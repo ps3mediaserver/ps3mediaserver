f2d75f1d895ad28e23473ac5afe19993b9e72be5
chocolateboy <chocolate@cpan.org>
2012-11-04 16:09:08 +0000

    clean up #90

--- src/main/java/net/pms/newgui/GeneralTab.java
+++ src/main/java/net/pms/newgui/GeneralTab.java
@@ -147,14 +147,14 @@ public class GeneralTab {
 						(JFrame) (SwingUtilities.getWindowAncestor((Component) PMS.get().getFrame())),
 						Messages.getString("NetworkTab.11") +
 						Messages.getString("NetworkTab.12"),
-						Messages.getString("Information"),
+						Messages.getString("Dialog.Information"),
 						JOptionPane.INFORMATION_MESSAGE);
 
 				} else {
 					JOptionPane.showMessageDialog(
 						(JFrame) (SwingUtilities.getWindowAncestor((Component) PMS.get().getFrame())),
 						Messages.getString("NetworkTab.14"),
-						Messages.getString("Error"),
+						Messages.getString("Dialog.Error"),
 						JOptionPane.ERROR_MESSAGE);
 				}
 			}
