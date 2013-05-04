b11a8e087f8fff792f8ca564da5b4b74c4ae2a14
Happy-Neko <happy.neko@gmail.com>
2013-05-05 00:20:16 +0400

    FFMpegVideo and FFMpegWebVideo merged with ums

--- src/main/java/net/pms/encoders/FFMpegWebVideo.java
+++ src/main/java/net/pms/encoders/FFMpegWebVideo.java
@@ -18,29 +18,49 @@
  */
 package net.pms.encoders;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import net.pms.configuration.PmsConfiguration;
 import net.pms.configuration.RendererConfiguration;
 import net.pms.dlna.DLNAMediaInfo;
 import net.pms.dlna.DLNAResource;
-import net.pms.encoders.FFMpegVideo;
 import net.pms.formats.Format;
+import net.pms.formats.FormatFactory;
+import net.pms.formats.WEB;
 import net.pms.io.OutputParams;
 import net.pms.io.PipeProcess;
 import net.pms.io.ProcessWrapper;
 import net.pms.io.ProcessWrapperImpl;
-
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.apache.commons.lang3.StringUtils.isNotEmpty;
 
 public class FFMpegWebVideo extends FFMpegVideo {
 	private static final Logger LOGGER = LoggerFactory.getLogger(FFMpegWebVideo.class);
-	private final PmsConfiguration configuration;
+	private static List<String> protocols;
+	public static PatternMap<Object> excludes = new PatternMap<Object>();
+
+	public static PatternMap<ArrayList> autoOptions = new PatternMap<ArrayList>() {
+		private static final long serialVersionUID = 5225786297932747007L;
+
+		@Override
+		public ArrayList add(String key, Object value) {
+			return put(key, (ArrayList) parseOptions((String) value));
+		}
+	};
+
+	public static PatternMap<String> replacements = new PatternMap<String>();
+	private static boolean init = false;
 
 	// FIXME we have an id() accessor for this; no need for the field to be public
 	@Deprecated
@@ -68,7 +88,31 @@ public class FFMpegWebVideo extends FFMpegVideo {
 
 	public FFMpegWebVideo(PmsConfiguration configuration) {
 		super(configuration);
-		this.configuration = configuration;
+
+		if (!init) {
+			readWebFilters(configuration.getProfileDirectory() + File.separator + "ffmpeg.webfilters");
+
+			protocols = FFmpegOptions.getSupportedProtocols(configuration);
+			// see XXX workaround below
+			protocols.add("mms");
+			protocols.add("https");
+			LOGGER.debug("FFmpeg supported protocols: " + protocols);
+
+			// Register protocols as a WEB format
+			final String[] ffmpegProtocols = protocols.toArray(new String[0]);
+			FormatFactory.getExtensions().add(0, new WEB() {
+				@Override
+				public String[] getId() {
+					return ffmpegProtocols;
+				}
+
+				@Override
+				public String toString() {
+					return "FFMPEG.WEB";
+				}
+			});
+			init = true;
+		}
 	}
 
 	@Override
@@ -81,9 +125,52 @@ public class FFMpegWebVideo extends FFMpegVideo {
 		params.minBufferSize = params.minFileSize;
 		params.secondread_minsize = 100000;
 		RendererConfiguration renderer = params.mediaRenderer;
+		File tempSubs = null;
+
+		if (!isDisableSubtitles(params)) {
+			tempSubs = getSubtitleFile(filename, media, params);
+		}
+
+		// XXX work around an ffmpeg bug: http://ffmpeg.org/trac/ffmpeg/ticket/998
+		if (filename.startsWith("mms:")) {
+			filename = "mmsh:" + filename.substring(4);
+		}
+
+		// check if we have modifier for this url
+		String r = replacements.match(filename);
+		if (r != null) {
+			filename = filename.replaceAll(r, replacements.get(r));
+			LOGGER.debug("modified url: " + filename);
+		}
+
+		FFmpegOptions customOptions = new FFmpegOptions();
+
+		// Gather custom options from various sources in ascending priority:
+		// - automatic options
+		String match = autoOptions.match(filename);
+		if (match != null) {
+			List<String> opts = autoOptions.get(match);
+			if (opts != null) {
+				customOptions.addAll(opts);
+			}
+		}
+		// - (http) header options
+		if (params.header != null && params.header.length > 0) {
+			String hdr = new String(params.header);
+			customOptions.addAll(parseOptions(hdr));
+		}
+		// - attached options
+		String attached = (String) dlna.getAttachment(ID);
+		if (attached != null) {
+			customOptions.addAll(parseOptions(attached));
+		}
+		// - renderer options
+		if (isNotEmpty(renderer.getCustomFFmpegOptions())) {
+			customOptions.addAll(parseOptions(renderer.getCustomFFmpegOptions()));
+		}
 
 		// basename of the named pipe:
-		// ffmpeg -loglevel warning -threads nThreads -i URL -threads nThreads -audio-channel-options -transcode-video-options /path/to/fifoName
+		// ffmpeg -loglevel warning -threads nThreads -i URL -threads nThreads -transcode-video-options /path/to/fifoName
 		String fifoName = String.format(
 			"ffmpegwebvideo_%d_%d",
 			Thread.currentThread().getId(),
@@ -94,60 +181,75 @@ public class FFMpegWebVideo extends FFMpegVideo {
 		PipeProcess pipe = new PipeProcess(fifoName);
 		pipe.deleteLater(); // delete the named pipe later; harmless if it isn't created
 		ProcessWrapper mkfifo_process = pipe.getPipeProcess();
-		// start the process as early as possible
+
+		// Start the process as early as possible
 		mkfifo_process.runInNewThread();
 
 		params.input_pipes[0] = pipe;
-		int nThreads = configuration.getNumberOfCpuCores();
 
-		// XXX work around an ffmpeg bug: http://ffmpeg.org/trac/ffmpeg/ticket/998
-		if (filename.startsWith("mms:")) {
-			filename = "mmsh:" + filename.substring(4);
-		}
-
-		// build the command line
-		List<String> cmdList = new ArrayList<String>();
-
-		cmdList.add(executable());
+		// Build the command line
+		final List<String> cmdList = new ArrayList<String>();
+		cmdList.add(executable().replace("/", "\\\\"));
 
 		// XXX squashed bug - without this, ffmpeg hangs waiting for a confirmation
 		// that it can write to a file that already exists i.e. the named pipe
 		cmdList.add("-y");
 
 		cmdList.add("-loglevel");
-		cmdList.add("warning"); // XXX this should probably be configurable, for debugging
 
-		// decoder threads
+		if (LOGGER.isTraceEnabled()) { // Set -loglevel in accordance with LOGGER setting
+			cmdList.add("info"); // Could be changed to "verbose" or "debug" if "info" level is not enough
+		} else {
+			cmdList.add("warning");
+		}
+
+		int nThreads = configuration.getNumberOfCpuCores();
+
+		// Decoder threads
 		cmdList.add("-threads");
 		cmdList.add("" + nThreads);
+
+		// Add global and input-file custom options, if any
+		if (!customOptions.isEmpty()) {
+			customOptions.transferGlobals(cmdList);
+			customOptions.transferInputFileOptions(cmdList);
+		}
 
 		cmdList.add("-i");
 		cmdList.add(filename);
 
-		// encoder threads
+		if (tempSubs == null) {
+			cmdList.addAll(getVideoFilterOptions(null, renderer, media));
+		} else {
+			cmdList.addAll(getVideoFilterOptions(tempSubs.getAbsolutePath(), renderer, media));
+		}
+
+		// Encoder threads
 		cmdList.add("-threads");
 		cmdList.add("" + nThreads);
 
-		// add video bitrate options (-b:a)
-		cmdList.addAll(getVideoBitrateOptions(filename, dlna, media, params));
+		// Add the output options (-f, -acodec, -vcodec)
+		cmdList.addAll(getTranscodeVideoOptions(renderer, media, params, null));
 
-		// add audio bitrate options (-b:v)
-		cmdList.addAll(getAudioBitrateOptions(filename, dlna, media, params));
+		// Add video bitrate options
+		cmdList.addAll(getVideoBitrateOptions(renderer, media));
 
-		// add audio channels (-ac)
-		cmdList.addAll(getAudioChannelOptions(filename, dlna, media, params));
+		// Add audio bitrate options
+		cmdList.addAll(getAudioBitrateOptions(renderer, media));
 
-		// add the output options (-f, -acodec, -vcodec)
-		cmdList.addAll(getVideoTranscodeOptions(filename, dlna, media, params));
+		// Add any remaining custom options
+		if (!customOptions.isEmpty()) {
+			customOptions.transferAll(cmdList);
+		}
 
-		// output file
+		// Output file
 		cmdList.add(pipe.getInputPipe());
 
-		// convert the command list to an array
-		String[] cmdArray = new String[ cmdList.size() ];
+		// Convert the command list to an array
+		String[] cmdArray = new String[cmdList.size()];
 		cmdList.toArray(cmdArray);
 
-		// hook to allow plugins to customize this command line
+		// Hook to allow plugins to customize this command line
 		cmdArray = finalizeTranscoderArgs(
 			filename,
 			dlna,
@@ -156,21 +258,20 @@ public class FFMpegWebVideo extends FFMpegVideo {
 			cmdArray
 		);
 
-		// now launch ffmpeg
+		// Now launch FFmpeg
 		ProcessWrapperImpl pw = new ProcessWrapperImpl(cmdArray, params);
-		pw.attachProcess(mkfifo_process); // clean up the mkfifo process when the transcode ends
+		pw.attachProcess(mkfifo_process); // Clean up the mkfifo process when the transcode ends
 
-		// give the mkfifo process a little time
+		// Give the mkfifo process a little time
 		try {
 			Thread.sleep(300);
 		} catch (InterruptedException e) {
 			LOGGER.error("Thread interrupted while waiting for named pipe to be created", e);
 		}
 
-		// launch the transcode command...
+		// Launch the transcode command...
 		pw.runInNewThread();
-
-		// ... and wait briefly to allow it to start
+		// ...and wait briefly to allow it to start
 		try {
 			Thread.sleep(200);
 		} catch (InterruptedException e) {
@@ -207,10 +308,112 @@ public class FFMpegWebVideo extends FFMpegVideo {
 			Format.Identifier id = format.getIdentifier();
 
 			if (id.equals(Format.Identifier.WEB)) {
-				return true;
+				String url = resource.getSystemName();
+				return protocols.contains(url.split(":")[0])
+					&& excludes.match(url) == null;
 			}
 		}
 
 		return false;
+	}
+
+	public boolean readWebFilters(String filename) {
+		PatternMap filter = null;
+		String line;
+		try {
+			LineIterator it = FileUtils.lineIterator(new File(filename));
+			try {
+				while (it.hasNext()) {
+					line = it.nextLine().trim();
+					if (line.isEmpty() || line.startsWith("#")) {
+						// continue
+					} else if (line.equals("EXCLUDE")) {
+						filter = excludes;
+					} else if (line.equals("OPTIONS")) {
+						filter = autoOptions;
+					} else if (line.equals("REPLACE")) {
+						filter = replacements;
+					} else if (filter != null) {
+						String[] var = line.split(" \\| ", 2);
+						filter.add(var[0], var.length > 1 ? var[1] : null);
+					}
+				}
+				return true;
+			} finally {
+				it.close();
+			}
+		} catch (Exception e) {
+			LOGGER.debug("Error reading ffmpeg web filters: " + e.getLocalizedMessage());
+		}
+		return false;
+	}
+}
+
+// A self-combining map of regexes that recompiles if modified
+class PatternMap<T> extends modAwareHashMap<String, T> {
+	private static final long serialVersionUID = 3096452459003158959L;
+	Matcher combo;
+	List<String> groupmap = new ArrayList<String>();
+
+	public T add(String key, Object value) {
+		return put(key, (T) value);
+	}
+
+	// Returns the first matching regex
+	String match(String str) {
+		if (!isEmpty()) {
+			if (modified) {
+				compile();
+			}
+			if (combo.reset(str).find()) {
+				for (int i = 0; i < combo.groupCount(); i++) {
+					if (combo.group(i + 1) != null) {
+						return groupmap.get(i);
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	void compile() {
+		String joined = "";
+		groupmap.clear();
+		for (String regex : this.keySet()) {
+			// add each regex as a capture group
+			joined += "|(" + regex + ")";
+			// map all subgroups to the parent
+			for (int i = 0; i < Pattern.compile(regex).matcher("").groupCount() + 1; i++) {
+				groupmap.add(regex);
+			}
+		}
+		// compile the combined regex
+		combo = Pattern.compile(joined.substring(1)).matcher("");
+		modified = false;
+	}
+}
+
+// A HashMap that reports whether it's been modified
+// (necessary because 'modCount' isn't accessible outside java.util)
+class modAwareHashMap<K, V> extends HashMap<K, V> {
+	private static final long serialVersionUID = -5334451082377480129L;
+	public boolean modified = false;
+
+	@Override
+	public void clear() {
+		modified = true;
+		super.clear();
+	}
+
+	@Override
+	public V put(K key, V value) {
+		modified = true;
+		return super.put(key, value);
+	}
+
+	@Override
+	public V remove(Object key) {
+		modified = true;
+		return super.remove(key);
 	}
 }
