5f0eb7bb417fbf4c9c0a582315403411128ca257
Happy-Neko <happy.neko@gmail.com>
2013-05-12 19:48:56 +0400

    removed unnecessary NPE catch

--- src/main/java/net/pms/dlna/CueFolder.java
+++ src/main/java/net/pms/dlna/CueFolder.java
@@ -169,10 +169,8 @@ public class CueFolder extends DLNAResource {
 					if (tracks.size() > 0 && addedResources.size() > 0) {
 						// last track
 						DLNAResource prec = addedResources.get(addedResources.size() - 1);
-						try {
-							prec.getSplitRange().setEnd(prec.getMedia().getDurationInSeconds());
-							prec.getMedia().setDuration(prec.getSplitRange().getDuration());
-						} catch (NullPointerException e) { }
+						prec.getSplitRange().setEnd(prec.getMedia().getDurationInSeconds());
+						prec.getMedia().setDuration(prec.getSplitRange().getDuration());
 						LOGGER.debug("Track #" + childrenNumber() + " split range: " + prec.getSplitRange().getStartOrZero() + " - " + prec.getSplitRange().getDuration());
 					}
 
