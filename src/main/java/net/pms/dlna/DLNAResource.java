2a7f96e801287f1ebb5499c356b4213964d02e56
chocolateboy <chocolate@cpan.org>
2013-06-13 16:52:38 +0100

    reduce logspam

--- src/main/java/net/pms/dlna/DLNAResource.java
+++ src/main/java/net/pms/dlna/DLNAResource.java
@@ -1575,9 +1575,9 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 								if (na > 2) { // No 5.1 dump in MPlayer
 									na = 2;
 								}
-								int finalsize = (int) (getMedia().getDurationInSeconds() * defaultFrequency * 2 * na);
-								logger.debug("Calculated size: " + finalsize);
-								addAttribute(sb, "size", finalsize);
+								int finalSize = (int) (getMedia().getDurationInSeconds() * defaultFrequency * 2 * na);
+								logger.trace("Calculated size for {}: {}", getSystemName(), finalSize);
+								addAttribute(sb, "size", finalSize);
 							}
 						}
 					} else {
